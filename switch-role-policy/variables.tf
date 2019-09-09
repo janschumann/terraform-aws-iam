801b9e52e6448629a5eb6ecaf3e8dd0fcc6ef455
chris <chris@weiss.is>
2019-09-09 12:40:51 +0200

    cleanup

--- switch-role-policy/variables.tf
+++ switch-role-policy/variables.tf
@@ -1,12 +1,15 @@
 variable "account_id" {
+  type        = string
   description = "The account to switch role to."
 }
 
 variable "account_name" {
+  type        = string
   description = "The account name to switch role to."
 }
 
 variable "role" {
+  type        = string
   description = "The role to switch to."
 }
 
