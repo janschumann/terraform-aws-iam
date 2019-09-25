73fb32a8aafa1eef2ce336947909f4712eab6398
Jan Schumann <js@schumann-it.com>
2019-09-25 19:14:58 +0200

    Packer & Kubernetes (#8)
    * packer refactoring & kubernetes cluster incl. jenkins service account

--- switch-role-policy/variables.tf
+++ switch-role-policy/variables.tf
@@ -1,19 +1,19 @@
 variable "account_id" {
   type        = string
-  description = "The account to switch role to."
+  description = "The account to switch role to"
 }
 
 variable "account_name" {
   type        = string
-  description = "The account name to switch role to."
+  description = "The account name to switch role to"
 }
 
 variable "role" {
   type        = string
-  description = "The role to switch to."
+  description = "The role to switch to"
 }
 
 variable "groups" {
   type        = list(string)
-  description = "A list of group names to allow role switch."
+  description = "A list of group names to allow role switch"
 }
